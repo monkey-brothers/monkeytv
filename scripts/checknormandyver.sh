ba81731d6338e1ad059d2f0fcaed0290f403ca5a
monkeytvjona <77692075+monkeytvjona@users.noreply.github.com>
2021-01-29 12:37:08 +0100

    Update checknormandyver.sh

--- scripts/checknormandyver.sh
+++ scripts/checknormandyver.sh
@@ -68,11 +68,11 @@ cp -r /tmp/NormandyEPG/datadocker/. /config/
 cp -r /tmp/NormandyEPG/Normandy_EPG.ver /config/NormandyEPG
 rm -rf /tmp/NormandyEPG/
 rm -rf /tmp/Normandy_EPG.zip
-if $instalado = "true"; then
+if [$instalado = "true"]; then
   s6-svc -r /var/run/s6/services/tvheadend/
   echo "Reiniciado servicio TVheadend"
 fi
-if $instalado = "false"; then
+if [$instalado = "false"]; then
   reboot
   echo "Reiniciado MonkeyTV"
 fi
