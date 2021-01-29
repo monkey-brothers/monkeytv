ecbe514ff92b5add85f7bfb18bade5e567da870b
monkeytvjona <77692075+monkeytvjona@users.noreply.github.com>
2021-01-29 20:11:04 +0100

    Update install_oscam.sh

--- scripts/install_oscam.sh
+++ scripts/install_oscam.sh
@@ -55,7 +55,8 @@
  groupmod -g 16 dialout && \
  usermod -a -G 16 abc && \
  svn checkout https://github.com/linuxserver/docker-oscam/trunk/root /tmp/docker-oscam/root && \
- mv /tmp/docker-oscam/root/etc/cont-init.d/30-config /tmp/docker-oscam/root/etc/cont-init.d/33-config && \
+ mv /tmp/docker-oscam/root/etc/cont-init.d/30-config /tmp/docker-oscam/root/etc/cont-init.d/33-oscam && \
+ rm -r /tmp/docker-oscam/root/.svn && \
  cp -r /tmp/docker-oscam/root/. / && \
  echo "**** cleanup ****" && \
  apk del --purge \
